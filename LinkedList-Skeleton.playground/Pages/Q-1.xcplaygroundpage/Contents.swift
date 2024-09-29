03ef6606f445e2cdc8f96d5207c8e636e6215bc3
“dhanon” <dhanon68@gmail.com>
2024-09-29 22:05:21 +0600

    fourth commit

--- LinkedList-Skeleton.playground/Pages/Q-1.xcplaygroundpage/Contents.swift
+++ LinkedList-Skeleton.playground/Pages/Q-1.xcplaygroundpage/Contents.swift
@@ -1,7 +1,31 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+/*
+ Write a function that determines the length of any linked list.
+ */
 
-var greeting = "Hello, playground"
+class Node {
+    var data: Int
+    var next: Node?
+    
+    init(_ data: Int, _ next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
 
-//: [Next](@next)
+func length(_ head: Node?) -> Int {
+    // Do your work here...
+    return 0
+}
+
+// 1 2 3 4 5 6
+let node6 = Node(6)
+let node5 = Node(5, node6)
+let node4 = Node(4, node5)
+let node3 = Node(3, node4)
+let node2 = Node(2, node3)
+let node1 = Node(1, node2)
+
+length(nil) // 0
+length(node1) // 6
